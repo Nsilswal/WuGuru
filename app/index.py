06b47f99d99b49c1a403d04cac7a275b89a2e235
ant33 <ant33@duke.edu>
2023-10-04 15:07:33 -0400

    fixed errors

--- app/index.py
+++ app/index.py
@@ -3,7 +3,7 @@ from flask_login import current_user
 import datetime
 
 from .models.recommendation import Recommendation
-from .models.purchase import Purchases
+from .models.purchase import Purchase
 
 from flask import Blueprint
 bp = Blueprint('index', __name__)
@@ -12,14 +12,16 @@ bp = Blueprint('index', __name__)
 @bp.route('/')
 def index():
     # get all user pruchases:
-    all_purchases = Purchases.get_all_by_uid_since(
-            current_user.id, datetime.datetime(1980, 9, 14, 0, 0, 0))
     # find the recs current user has published:
     if current_user.is_authenticated:
         reccs = Recommendation.get_all_by_uid_since(
             current_user.id, datetime.datetime(1980, 9, 14, 0, 0, 0))
+        all_purchases = Purchase.get_all_by_uid_since(
+            current_user.id, datetime.datetime(1980, 9, 14, 0, 0, 0))
     else:
         reccs = None
+        all_purchases = None
+        return render_template('base.html')
     # render the page by adding information to the index.html file
     return render_template('index.html',
                            all_purch=all_purchases,
