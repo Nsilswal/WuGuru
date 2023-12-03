f215f93bba5e79e3b09b122f6fa21a5f67719dd2
Mia Malden <mm871@duke.edu>
2023-12-03 16:43:16 -0500

    food item add form created and linked from index

--- app/fooditems.py
+++ app/fooditems.py
@@ -3,7 +3,7 @@ from flask import jsonify, redirect, url_for, flash, render_template, request, s
 from flask import Flask, Blueprint
 import os
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, FileField
+from wtforms import StringField, SubmitField, FileField, FloatField, IntegerField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 from datetime import datetime
 
@@ -30,7 +30,32 @@ def fooditems_search():
     fooditems = Fooditem.search_by_keyword(keyword)
     return render_template('fooditem_home.html', title="Food Item Home", avail_fi = fooditems)
 
+@bp.route('/fooditems/add',methods=['POST'])
+def fooditems_add():
+    form = AddForm()
+    if form.validate_on_submit():
+        restuarantID = current_user.restaurantOwned
+        Fooditem.register(form.name.data,form.price.data,form.protein.data,
+        form.sugars.data, form.fats.data, forms.calories.data, 
+        forms.allergens.data, restaurantID, forms.diet.data)
+        return redirect(url_for('index.index'))
+    return render_template('add_fooditem.html',title='Add a Menu Item', form = form)
+    
+
+
 class SearchForm(FlaskForm):
         title = StringField('Food Item name',validators=[DataRequired()])
         search = SubmitField('Search')
+
+class AddForm(FlaskForm):
+        name = StringField('Add name', validators=[DataRequired()])
+        protein = FloatField('Protein (g):', validators=[DataRequired()])
+        sugars = FloatField('Sugars:', validators=[DataRequired()])
+        fats = FloatField('Fats:', validators=[DataRequired()])
+        price = FloatField('Price:', validators=[DataRequired()])
+        calories = IntegerField('Calories:', validators=[DataRequired()])
+        allergens = StringField('Add allergens:', validators = [DataRequired()])
+        diet = StringField('Add dietary restriction')
+        submit = SubmitField('Submit')
+
 
